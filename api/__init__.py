9cc5e46ce3224547d0705364731e2c18dd4dc9de
Micheal <user@Micheal.lan>
2023-03-16 00:00:40 +0100

    signup and login auth

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,14 @@
 
 
 from flask import Flask
+# from flask import app
 from flask_restx import Api
 from .resources.views import portal_namespace
 from .auth.views import auth_namespace
 from .config.config import config_dict
+from .utils import db
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 
 
@@ -13,6 +17,26 @@ def create_app(config=config_dict['dev']):
     app = Flask(__name__)
 
     app.config.from_object(config)
+
+    # app.config.from_mapping(
+    #     DATABASE_URI="postgres://postgres:piperlead@127.0.0.1:5432/student_db"
+    # )
+
+    app.config.from_object(config)
+    db.init.app(app)
+
+    jwt = JWTManager(app)
+
+    migrate = Migrate(app, db)
+
+    authorizations = {
+        "Bearer Auth": {
+            "type": "apiKey",
+            "in": "header",
+            "name": "Authorization",
+            "description": "Add a JWT token to the header with ** Bearer &lt;JWT&gt; ** token to authorize"
+        }
+    }
 
 
 
